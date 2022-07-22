32d41e887f9313b83be526d9774a0f1594916d0b
Sławomir Świętoniowski <60968459+sswietoniowski@users.noreply.github.com>
2022-07-22 16:03:35 +0200

    Retrieved file from the API

---
+++ CityInfo.API/Controllers/FilesController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.StaticFiles;
+
+namespace CityInfo.API.Controllers
+{
+    [Route("api/files")]
+    [ApiController]
+    public class FilesController : ControllerBase
+    {
+        private readonly FileExtensionContentTypeProvider _fileExtensionContentTypeProvider;
+
+        public FilesController(FileExtensionContentTypeProvider fileExtensionContentTypeProvider)
+        {
+            _fileExtensionContentTypeProvider = fileExtensionContentTypeProvider
+                ?? throw new ArgumentNullException(nameof(fileExtensionContentTypeProvider));
+        }
+
+        [HttpGet("{fileid}")]
+        public ActionResult GetFile(string fileId)
+        {
+            //FileContentResult result = new FileContentResult(new byte[0], "application/octet-stream");
+            //FileStreamResult result = new FileStreamResult(new MemoryStream(new byte[0]), "application/octet-stream");
+
+            var pathToFile = "test.txt";
+
+            if (!System.IO.File.Exists(pathToFile))
+            {
+                return NotFound();
+            }
+
+            if (!_fileExtensionContentTypeProvider.TryGetContentType(pathToFile, out var contentType))
+            {
+                contentType = "application/octet-stream";
+            }
+
+            var bytes = System.IO.File.ReadAllBytes(pathToFile);
+
+            return File(bytes, contentType, Path.GetFileName(pathToFile));
+        }
+    }
+}
--- CityInfo.API/Program.cs
+++ CityInfo.API/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.AspNetCore.StaticFiles;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,8 @@ builder.Services.AddControllers(options =>
 {
     options.ReturnHttpNotAcceptable = true;
 }).AddXmlDataContractSerializerFormatters();
+
+builder.Services.AddSingleton<FileExtensionContentTypeProvider>();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
