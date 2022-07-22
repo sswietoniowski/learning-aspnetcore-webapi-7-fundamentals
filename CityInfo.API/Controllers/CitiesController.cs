19f859dd1d5bdaf413142ee2acd480c66fa10c02
Sławomir Świętoniowski <60968459+sswietoniowski@users.noreply.github.com>
2022-07-22 15:44:09 +0200

    Added support for the XML formatter

--- CityInfo.API/Controllers/CitiesController.cs
+++ CityInfo.API/Controllers/CitiesController.cs
@@ -21,7 +21,7 @@ namespace CityInfo.API.Controllers
             // Level 100 - Informational
             // Level 200 - OK (200 OK, 201 Created, 204 No Content)
             // Level 300 - Redirected
-            // Level 400 - Client's Error (400 Bad Request, 401 Not Authorized, 403 Forbidden, 404 Not Found, 409 Conflict)
+            // Level 400 - Client's Error (400 Bad Request, 401 Not Authorized, 403 Forbidden, 404 Not Found, 406 Not Acceptable, 409 Conflict)
             // Level 500 - Server's Error (500 Internal Server Error)
 
             var city = CitiesDataStore.Current.Cities.FirstOrDefault(city => city.Id == id);
