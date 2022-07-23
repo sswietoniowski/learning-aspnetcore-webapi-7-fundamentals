cbba9d2346e312e8c1bb27a6b8a7c1943bece242
Sławomir Świętoniowski <60968459+sswietoniowski@users.noreply.github.com>
2022-07-23 18:52:46 +0200

    Fixed names to adhere to a proper convention

--- CityInfo.API/DataAccess/Repositories/ICityInfoRepository.cs
+++ CityInfo.API/DataAccess/Repositories/ICityInfoRepository.cs
@@ -8,9 +8,9 @@ namespace CityInfo.API.DataAccess.Repositories
     {
         // Whether we would return IEnumerable or IQueryable (and by that we would give the developer more options to query the data at the expense of leaking the
         // query logic) is a matter of discussion.
-        Task<IEnumerable<City>> GetCities();
-        Task<City> GetCity(int cityId, bool includePointsOfInterest);
-        Task<IEnumerable<PointOfInterest>> GetPointsOfInterestForCity(int cityId);
-        Task<PointOfInterest> GetPointOfInterestForCity(int cityId, int pointOfInterestId);
+        Task<IEnumerable<City>> GetCitiesAsync();
+        Task<City> GetCityAsync(int cityId, bool includePointsOfInterest);
+        Task<IEnumerable<PointOfInterest>> GetPointsOfInterestForCityAsync(int cityId);
+        Task<PointOfInterest> GetPointOfInterestForCityAsync(int cityId, int pointOfInterestId);
     }
 }
