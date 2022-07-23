a01575925a241eefaa42cdb6ac622e0780958057
Sławomir Świętoniowski <60968459+sswietoniowski@users.noreply.github.com>
2022-07-23 18:53:18 +0200

    Fixed nullability

--- CityInfo.API/DataAccess/Repositories/ICityInfoRepository.cs
+++ CityInfo.API/DataAccess/Repositories/ICityInfoRepository.cs
@@ -9,8 +9,8 @@ namespace CityInfo.API.DataAccess.Repositories
         // Whether we would return IEnumerable or IQueryable (and by that we would give the developer more options to query the data at the expense of leaking the
         // query logic) is a matter of discussion.
         Task<IEnumerable<City>> GetCitiesAsync();
-        Task<City> GetCityAsync(int cityId, bool includePointsOfInterest);
+        Task<City?> GetCityAsync(int cityId, bool includePointsOfInterest);
         Task<IEnumerable<PointOfInterest>> GetPointsOfInterestForCityAsync(int cityId);
-        Task<PointOfInterest> GetPointOfInterestForCityAsync(int cityId, int pointOfInterestId);
+        Task<PointOfInterest?> GetPointOfInterestForCityAsync(int cityId, int pointOfInterestId);
     }
 }
