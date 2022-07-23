b97b3e22f2c5b6c825fb847f03b5add006f60abd
Sławomir Świętoniowski <60968459+sswietoniowski@users.noreply.github.com>
2022-07-23 16:44:37 +0200

    Added info about alternative way of handling null warnings

--- CityInfo.API/DataAccess/DbContexts/CityInfoDbContext/CityInfoDbContext.cs
+++ CityInfo.API/DataAccess/DbContexts/CityInfoDbContext/CityInfoDbContext.cs
@@ -7,7 +7,9 @@ namespace CityInfo.API.DataAccess.DbContexts.CityInfoDbContext
     public class CityInfoDbContext : DbContext
     {
         public DbSet<City> Cities => Set<City>();
-        public DbSet<PointOfInterest> PointsOfInterest  => Set<PointOfInterest>();
+        public DbSet<PointOfInterest> PointsOfInterest => Set<PointOfInterest>();
+        // Alternatively: to eliminate null warnings, given that these properties won't be null, we might use:
+        // public DbSet<PointOfInterest> PointsOfInterest { get; set; } = null!
 
         public CityInfoDbContext(DbContextOptions<CityInfoDbContext> options)
             : base(options)
