208079c2b7edbcb001b435468563409f23368dfb
Sławomir Świętoniowski <60968459+sswietoniowski@users.noreply.github.com>
2022-07-23 18:51:08 +0200

    Added alternative implementation of the repository

---
+++ CityInfo.API/DataAccess/Repositories/ICityInfoRepository.cs
@@ -0,0 +1,16 @@
+﻿using CityInfo.API.DataAccess.Entities;
+using CityInfo.API.DTOs;
+
+namespace CityInfo.API.DataAccess.Repositories
+{
+    // Alternative approach to the way how the "repository pattern" can be structured
+    public interface ICityInfoRepository
+    {
+        // Whether we would return IEnumerable or IQueryable (and by that we would give the developer more options to query the data at the expense of leaking the
+        // query logic) is a matter of discussion.
+        Task<IEnumerable<City>> GetCities();
+        Task<City> GetCity(int cityId, bool includePointsOfInterest);
+        Task<IEnumerable<PointOfInterest>> GetPointsOfInterestForCity(int cityId);
+        Task<PointOfInterest> GetPointOfInterestForCity(int cityId, int pointOfInterestId);
+    }
+}
